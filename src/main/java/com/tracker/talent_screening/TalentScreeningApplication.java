87a33fea10d03adf6f0d241a6d4f3d97448c8f30
oabdelhamed <oabdelhamed@alriyadh.gov.sa>
2025-05-19 22:28:09 +0300

    Initial commit

---
+++ src/main/java/com/tracker/talent_screening/TalentScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.talent_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentScreeningApplication.class, args);
+	}
+
+}
